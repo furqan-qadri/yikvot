2a6520a4c879f0a75a918e5be49ff3a222fd836e
furqan-qadri <furqaanqadri@gmail.com>
2024-09-14 13:41:51 +0800

    update links

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { BookOpen, Search, ChevronRight } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
@@ -22,16 +23,17 @@ export default function Home() {
           scrollY > 80 ? "bg-[#ED4141] shadow-md" : "bg-transparent"
         }`}
       >
-        <div className="container mx-auto px-4">
+        <div className="container mx-auto">
           <nav className="flex items-center justify-between p-4">
             <Link
               href="/"
-              className="text-white transition-colors text-3xl font-bold hover:text-red-600"
+              className="text-white transition-colors text-2xl xl:text-3xl font-bold hover:text-red-600"
             >
               Yikvot
             </Link>
-            <ul className="flex space-x-8">
-              {/* <li>
+            <div>
+              <ul className="flex space-x-8">
+                {/* <li>
                 <Link
                   href="/"
                   className="text-red-800 hover:text-red-600 transition-colors"
@@ -39,31 +41,32 @@ export default function Home() {
                   Home
                 </Link>
               </li> */}
-              <li>
-                <Link
-                  href="/books"
-                  className="text-white transition-colors text-xl font-bold hover:text-red-600"
-                >
-                  Books
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/about"
-                  className="text-white transition-colors text-xl font-bold"
-                >
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/contact"
-                  className="text-white transition-colors text-xl font-bold"
-                >
-                  Contact
-                </Link>
-              </li>
-            </ul>
+                <li>
+                  <Link
+                    href="/"
+                    className="text-white transition-colors text-lg xl:text-xl font-bold hover:text-red-600"
+                  >
+                    Books
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/"
+                    className="text-white transition-colors text-lg xl:text-xl font-bold"
+                  >
+                    About
+                  </Link>
+                </li>
+                <li className=" hidden">
+                  <Link
+                    href="/contact"
+                    className="text-white transition-colors text-xl font-bold"
+                  >
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </nav>
         </div>
       </header>
@@ -77,7 +80,7 @@ export default function Home() {
           <div className="container mx-auto px-4 z-10 mt-10">
             <div className="flex flex-col md:flex-row items-center">
               <div className="md:w-1/2 text-white">
-                <h1 className="text-5xl md:text-7xl font-bold mb-6 leading-tight">
+                <h1 className="text-5xl md:text-7xl font-bold mb-6 leading-tight mt-8">
                   Discover Kashmir's Literary Treasures
                 </h1>
                 <p className="text-2xl md:text-2xl mb-8 max-w-lg">
@@ -90,7 +93,14 @@ export default function Home() {
                 </Button>
               </div>
               <div className="md:w-1/2 mt-12 md:mt-0">
-                <AnimatedLeaf />
+                {/* <AnimatedLeaf /> */}
+                <Image
+                  src="/leaf.webp"
+                  width={500}
+                  height={500}
+                  alt="Picture of the author"
+                  className="rounded-lg"
+                />
               </div>
             </div>
           </div>
@@ -102,11 +112,11 @@ export default function Home() {
               Our Mission
             </h2>
             <p className="text-xl text-gray-700 max-w-3xl mx-auto text-center leading-relaxed">
-              The Kashmir Literary Archive is dedicated to preserving and
-              sharing the rich literary traditions of Kashmir. Our digital
-              collection makes centuries of Kashmiri literature accessible to
-              readers and researchers worldwide, fostering a deeper
-              understanding and appreciation of Kashmir's cultural heritage.
+              Yikvot is dedicated to preserving and sharing the rich literary
+              traditions of Kashmir. Our digital collection makes centuries of
+              Kashmiri literature accessible to readers and researchers
+              worldwide, fostering a deeper understanding and appreciation of
+              Kashmir's cultural heritage.
             </p>
           </div>
         </section>
@@ -171,11 +181,10 @@ export default function Home() {
         <div className="container mx-auto px-4">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="mb-8 md:mb-0">
-              <h3 className="text-2xl font-bold mb-4">
-                Kashmir Literary Archive
-              </h3>
+              <h3 className="text-2xl font-bold mb-4">Yikvot</h3>
               <p className="text-red-200">
-                Preserving the literary heritage of Kashmir
+                Preserving the literary heritage of Kashmir <br /> <br />
+                #KaeshirSeetiKasehirSaeri
               </p>
             </div>
             <nav className="mb-8">
